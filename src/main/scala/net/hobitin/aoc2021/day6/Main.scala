2808ca4d83c14f6882c96e3579fe49a04acd9cc4
Lukas Skala <indy@hobitin.net>
2021-12-06 15:09:32 +0100

    Switch to for cycles

--- src/main/scala/net/hobitin/aoc2021/day6/Main.scala
+++ src/main/scala/net/hobitin/aoc2021/day6/Main.scala
@@ -6,11 +6,11 @@ object Main extends TaskMixin{
   def main(args: Array[String]): Unit = {
     val fish = SchoolOfFish.build(input().head)
 
-    (0 until 80).foreach(_ => fish.nextDay())
+    for (_ <- 0 until 80) fish.nextDay()
 
     printFirst(fish.count)
 
-    (0 until 176).foreach(_ => fish.nextDay())
+    for (_ <- 0 until 176) fish.nextDay()
 
     printSecond(fish.count)
   }
