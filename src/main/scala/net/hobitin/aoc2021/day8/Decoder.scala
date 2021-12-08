8c1d1d0ff7146df457f27883bb990fe912734817
Lukas Skala <indy@hobitin.net>
2021-12-08 15:38:06 +0100

    day8

---
+++ src/main/scala/net/hobitin/aoc2021/day8/Decoder.scala
@@ -0,0 +1,75 @@
+package net.hobitin.aoc2021.day8
+
+case class Decoder private (number: Int) {
+
+}
+
+object Decoder {
+  def apply(input: String): Decoder = {
+    val inputArray = input.split("\\|")
+    val digits = inputArray(0)
+      .split(" ")
+      .filter(_.nonEmpty)
+      .map(digit => digit.toCharArray.sorted.foldLeft("")(_+_))
+
+    val one = digits.filter(_.length == 2).head
+    val four = digits.filter(_.length == 4).head
+    val seven = digits.filter(_.length == 3).head
+    val eight = digits.filter(_.length == 7).head
+
+    val sixLineDigits = digits.filter(_.length == 6)
+
+    val six = sixLineDigits
+      .filter(digit => !digit.contains(one(0)) || !digit.contains(one(1)))
+      .head
+
+    val nine = sixLineDigits
+      .filter(digit => four.toCharArray.forall(digit.contains(_)))
+      .head
+
+    val zero = sixLineDigits
+      .filter(six != _)
+      .filter(nine != _)
+      .head
+
+    val fiveLineDigits = digits.filter(_.length == 5)
+
+    val three = fiveLineDigits
+      .filter(digit => digit.contains(one(0)) && digit.contains(one(1)))
+      .head
+
+    val lines = "abcdefg".toCharArray
+    val sixMissingLine = lines.filter(!six.contains(_)).head
+
+    val two = fiveLineDigits
+      .filter(three != _)
+      .filter(_.contains(sixMissingLine))
+      .head
+
+    val five = fiveLineDigits
+      .filter(three != _)
+      .filter(two != _)
+      .head
+
+    val number = inputArray(1)
+      .trim
+      .split(" ")
+      .map(digit => digit.toCharArray.sorted.foldLeft("")(_+_))
+      .map {
+        case x if zero.equals(x) => "0"
+        case x if one.equals(x) => "1"
+        case x if two.equals(x) => "2"
+        case x if three.equals(x) => "3"
+        case x if four.equals(x) => "4"
+        case x if five.equals(x) => "5"
+        case x if six.equals(x) => "6"
+        case x if seven.equals(x) => "7"
+        case x if eight.equals(x) => "8"
+        case x if nine.equals(x) => "9"
+      }
+      .reduce(_+_)
+      .toInt
+
+    Decoder(number)
+  }
+}
---
+++ src/main/scala/net/hobitin/aoc2021/day8/Main.scala
@@ -0,0 +1,28 @@
+package net.hobitin.aoc2021.day8
+
+import net.hobitin.TaskMixin
+
+object Main extends TaskMixin {
+  def main(args: Array[String]): Unit = {
+    val outputCount = input
+      .map(_.split("\\|"))
+      .map(_.tail.head)
+      .flatMap(_.split(" "))
+      .map(_.length)
+      .count(value => {
+        value match {
+          case 2 | 3 | 4 | 7 => true
+          case _ => false
+        }
+      })
+
+    printFirst(outputCount)
+
+    val sum = input
+      .map(Decoder(_))
+      .map(_.number)
+      .sum
+
+    printSecond(sum)
+  }
+}
