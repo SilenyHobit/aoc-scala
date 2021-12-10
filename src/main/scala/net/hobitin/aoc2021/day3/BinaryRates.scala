a93686d39a3771bbb5848b45ce28b63eb3788329
Lukas Skala <indy@hobitin.net>
2021-12-10 19:22:23 +0100

    Remove duplicate code

--- src/main/scala/net/hobitin/aoc2021/day3/BinaryRates.scala
+++ src/main/scala/net/hobitin/aoc2021/day3/BinaryRates.scala
@@ -9,9 +9,9 @@ class BinaryRates {
   def process(input: String): BinaryRates = {
     commonality match {
       case None => commonality = Some(input.toCharArray.zip(new Array[Int](input.length))
-          .map(pair => if(pair._1 == '0') pair._2-1 else pair._2+1))
+        .map(pair => if (pair._1 == '0') pair._2 - 1 else pair._2 + 1))
       case Some(array) => commonality = Some(input.toCharArray.zip(array)
-        .map(pair => if(pair._1 == '0') pair._2-1 else pair._2+1))
+        .map(pair => if (pair._1 == '0') pair._2 - 1 else pair._2 + 1))
     }
 
     this
@@ -19,56 +19,39 @@ class BinaryRates {
 
   def gammaRate: Int = {
     val gamma = commonality.get
-      .map(number => if (number<0) "0" else "1")
-      .reduceLeft(_+_)
+      .map(number => if (number < 0) "0" else "1")
+      .reduceLeft(_ + _)
     Integer.parseInt(gamma, 2)
   }
 
   def epsilonRate: Int = {
     val epsilon = commonality.get
-      .map(number => if (number>0) "0" else "1")
-      .reduceLeft(_+_)
+      .map(number => if (number > 0) "0" else "1")
+      .reduceLeft(_ + _)
     Integer.parseInt(epsilon, 2)
   }
 
-  def findOxygenRating(input: Seq[String]): Int = findOxygenRating(input, 0, commonality.get)
+  def findOxygenRating(input: Seq[String]): Int = findRating(input, 0, commonality.get, '0', '1')
+
+  def findScrubberRating(input: Seq[String]): Int = findRating(input, 0, commonality.get, '1', '0')
 
   @tailrec
-  private def findOxygenRating(input: Seq[String], index: Int, localCommonality: Array[Int]): Int = {
-    if (input.size == 1)
-      return Integer.parseInt(input.head, 2)
+  private def findRating(input: Seq[String], index: Int, localCommonality: Array[Int], zeroMostCommon: Char, oneMostCommon: Char): Int = {
+    if (input.size == 1) {
+      Integer.parseInt(input.head, 2)
+    } else {
+      val (filtered, removed) = input.partition(value => {
+        (localCommonality(index) < 0 && value.charAt(index) == zeroMostCommon) ||
+          (localCommonality(index) >= 0 && value.charAt(index) == oneMostCommon)
+      })
 
-    val (filtered, removed) = input.partition(value => {
-      (localCommonality(index) < 0 && value.charAt(index) == '0') ||
-        (localCommonality(index) >= 0 && value.charAt(index) == '1')
-    })
+      val updatedCommonality = removed.foldLeft(localCommonality)((array, value) => {
+        value.toCharArray.zip(array)
+          .map(pair => if (pair._1 == '0') pair._2 + 1 else pair._2 - 1)
+      })
 
-    val updatedCommonality = removed.foldLeft(localCommonality)((array, value) => {
-      value.toCharArray.zip(array)
-        .map(pair => if(pair._1 == '0') pair._2+1 else pair._2-1)
-    })
-
-    findOxygenRating(filtered, index+1, updatedCommonality)
-  }
-
-  def findScrubberRating(input: Seq[String]): Int = findScrubberRating(input, 0, commonality.get)
-
-  @tailrec
-  private def findScrubberRating(input: Seq[String], index: Int, localCommonality: Array[Int]): Int = {
-    if (input.size == 1)
-      return Integer.parseInt(input.head, 2)
-
-    val (filtered, removed) = input.partition(value => {
-      (localCommonality(index) < 0 && value.charAt(index) == '1') ||
-        (localCommonality(index) >= 0 && value.charAt(index) == '0')
-    })
-
-    val updatedCommonality = removed.foldLeft(localCommonality)((array, value) => {
-      value.toCharArray.zip(array)
-        .map(pair => if(pair._1 == '0') pair._2+1 else pair._2-1)
-    })
-
-    findScrubberRating(filtered, index+1, updatedCommonality)
+      findRating(filtered, index + 1, updatedCommonality, zeroMostCommon, oneMostCommon)
+    }
   }
 
 }
