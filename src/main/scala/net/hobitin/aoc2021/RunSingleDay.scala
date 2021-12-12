8c3283009f760e4b19f380adf38895b722ff23cc
Lukas Skala <indy@hobitin.net>
2021-12-12 18:20:56 +0100

    Add a warmup run

--- src/main/scala/net/hobitin/aoc2021/RunSingleDay.scala
+++ src/main/scala/net/hobitin/aoc2021/RunSingleDay.scala
@@ -3,7 +3,6 @@ package net.hobitin.aoc2021
 import net.hobitin.Day
 import net.hobitin.aoc2021.day12.Day12
 
-import java.time.temporal.ChronoUnit
 import java.time.{Duration, Instant}
 
 object RunSingleDay {
@@ -16,6 +15,10 @@ object RunSingleDay {
   }
 
   def runSingleDay(day: Day, runs: Int = 1): Duration = {
+    //warmup
+    day.firstTask
+    day.secondTask
+
     val result = (0 until runs)
       .map(_ => {
         val firstStart = Instant.now()
@@ -30,15 +33,15 @@ object RunSingleDay {
       })
       .reduce((tuple1, tuple2) => (tuple1._1, tuple1._2, tuple1._3 + tuple2._3, tuple1._4 + tuple2._4))
 
-    val firstDuration = Duration.ofNanos(result._3/runs)
+    val firstDuration = Duration.ofNanos(result._3 / runs)
 
-    val secondDuration = Duration.ofNanos(result._4/runs)
+    val secondDuration = Duration.ofNanos(result._4 / runs)
 
     val duration = firstDuration.plus(secondDuration)
 
-    println(f"| ${day.name}%-30s | ${duration.toSecondsPart}%21d.${duration.toNanosPart/1000}%06dS |")
-    println(f"| ${"    Part 1"}%-30s | ${result._1}%15s | ${firstDuration.toSecondsPart}%3d.${firstDuration.toNanosPart/1000}%06dS |")
-    println(f"| ${"    Part 2"}%-30s | ${result._2}%15s | ${secondDuration.toSecondsPart}%3d.${secondDuration.toNanosPart/1000}%06dS |")
+    println(f"| ${day.name}%-30s | ${duration.toSecondsPart}%21d.${duration.toNanosPart / 1000}%06dS |")
+    println(f"| ${"    Part 1"}%-30s | ${result._1}%15s | ${firstDuration.toSecondsPart}%3d.${firstDuration.toNanosPart / 1000}%06dS |")
+    println(f"| ${"    Part 2"}%-30s | ${result._2}%15s | ${secondDuration.toSecondsPart}%3d.${secondDuration.toNanosPart / 1000}%06dS |")
     println("------------------------------------------------------------------")
 
     duration
